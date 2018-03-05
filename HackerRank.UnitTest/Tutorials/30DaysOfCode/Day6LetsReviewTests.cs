b9d6c60eac1f5b56e3b5d5ee2045fc076f3fe871
Kevy Augusto <kevyaugusto@gmail.com>
2018-03-05 18:39:37 +1100

    Day6 implemented

---
+++ HackerRank.UnitTest/Tutorials/30DaysOfCode/Day6LetsReviewTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.UnitTest.Tutorials._30DaysOfCode
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class Day6LetsReviewTests
+    {
+        [TestMethod]
+        public void Day6LetsReviewTest00()
+        {
+            var methodName = System.Reflection.MethodInfo.GetCurrentMethod().Name;
+
+            var inputs = Utils.ReadInput(methodName);
+
+            var results = new HackerRank.Tutorials._30DaysOfCode.Day6LetsReview().Main(inputs);
+
+            Utils.CompareOutputs(results, methodName);
+        }
+    }
+}
---
+++ HackerRank/Tutorials/30DaysOfCode/Day6LetsReview.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Tutorials._30DaysOfCode
+{
+    public class Day6LetsReview
+    {
+        public List<string> Main(List<string> args)
+        {
+            var result = new List<string>();
+
+            var n = int.Parse(args[0]);
+
+            for (int i = 0; i < n; i++)
+            {
+                var s = args[i + 1];
+
+                string even = string.Empty, odd = string.Empty;
+
+                for (int j = 0; j < s.Length; j++)
+                {
+                    var c = s[j];
+
+                    if (j % 2 == 0)
+                        even += c;
+                    else
+                        odd += c;
+                }
+
+                result.Add($"{even} {odd}");
+            }
+
+            return result;
+        }
+
+        public void Main(string[] args)
+        {
+            var result = new List<string>();
+
+            var n = int.Parse(args[0]);
+
+            for (int i = 0; i < n; i++)
+            {
+                var s = args[i + 1];
+
+                string even = string.Empty, odd = string.Empty;
+
+                for (int j = 0; j < s.Length; j++)
+                {
+                    var c = s[j];
+
+                    if (j % 2 == 0)
+                        even += c;
+                    else
+                        odd += c;
+                }
+
+                result.Add($"{even} {odd}");
+            }
+
+            return result;
+        }
+    }
+}
