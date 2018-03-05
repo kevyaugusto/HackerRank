3d55886a7d40523ec74df90e8332ead80dfcb00d
Kevy Augusto <kevyaugusto@gmail.com>
2018-03-05 20:12:00 +1100

    Day 9 Recursion implemented successfully

---
+++ HackerRank.UnitTest/Tutorials/30DaysOfCode/Day9RecursionTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.UnitTest.Tutorials._30DaysOfCode
+{
+    [TestClass]
+    public class Day9RecursionTests
+    {
+        [TestMethod]
+        public void Day9RecursionTest00()
+        {
+            var methodName = System.Reflection.MethodInfo.GetCurrentMethod().Name;
+            
+            var inputs = Utils.ReadInput(methodName);
+
+            var results = new HackerRank.Tutorials._30DaysOfCode.Day9Recursion().Main(inputs);
+
+            Utils.CompareOutputs(results, methodName);
+        }
+    }
+}
---
+++ HackerRank/Tutorials/30DaysOfCode/Day9Recursion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Tutorials._30DaysOfCode
+{
+    public class Day9Recursion
+    {
+        public List<string> Main(List<string> args)
+        {
+            var n = int.Parse(args[0]);
+
+            var output = Factorial(n);
+
+            return new List<string>() { output.ToString() };
+        }
+
+        private int Factorial(int n)
+        {
+            if (n == 1)
+                return n;
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
